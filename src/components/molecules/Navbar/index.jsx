25b68701dfe372f89d91d05e6f1c41d018265773
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-23 03:02:08 +0100

    Switch up theme, add navbar title and button

--- src/components/molecules/Navbar/index.jsx
+++ src/components/molecules/Navbar/index.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import './Navbar.scss'
 
 const Navbar = () => {
-  return <div className='navbar'>
-    <div className="logo">F-U</div>
-  </div>
+  return (
+    <div className='navbar'>
+      <div className='menu'>
+        <svg
+          width='32'
+          height='32'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+          <path d='M4 6l16 0' />
+          <path d='M4 12l16 0' />
+          <path d='M4 18l16 0' />
+        </svg>
+      </div>
+      <div className='app-title'>FjaloriUrban</div>
+    </div>
+  )
 }
 
 export default Navbar
