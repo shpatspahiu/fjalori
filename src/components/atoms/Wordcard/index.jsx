20ab63956b606d4cef6b4b6c80e7c08c01c2592f
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-23 02:31:59 +0100

    Fully working wordcard
    Added: styling, dynamic data, dynamic card

--- src/components/atoms/Wordcard/index.jsx
+++ src/components/atoms/Wordcard/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import './Wordcard.scss'
 
-const Wordcard = () => {
-  return <div className='wordcard'>Workcard</div>
+const Wordcard = ({ title, examples, explanation, contribution }) => {
+  return (
+    <div className='wordcard'>
+      <div className='title'>{title}</div>
+      {explanation && <div className='explanation'>{explanation}</div>}
+      {!!examples?.length && (
+        <div className='example-container'>
+          {examples?.map((eg, i) => {
+            return (
+              <div key={i} className='example'>
+                {eg}
+              </div>
+            )
+          })}
+        </div>
+      )}
+      {contribution && (
+        <div className='contribution'>
+          by <span>{contribution?.username}</span> {contribution?.timeOf}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Wordcard
--- src/components/organisms/Home/index.jsx
+++ src/components/organisms/Home/index.jsx
@@ -3,26 +3,40 @@ import './Home.scss'
 import WordCard from '../../atoms/Wordcard'
 
 const Home = () => {
-  const title = 'Kaka'
-  const example = 'When you take a good shit'
-  const arrOfExplenations = [
-    'explenation1 is going to be a bit longer in order to see how it behaves with card!',
-    'example 2 is ex 2 ',
-    "example 3 we'll see, need to use the ... operator"
-  ]
-  const mockPerson = {
-    name: 'Shpati',
-    username: 'spahiush',
-    time_of_contribution: '15.04.1999'
+  const card = {
+    title: 'Kaka',
+    explanation: 'When you take a good shit',
+    arrOfExamples: [
+      'explenation1 is going to be a bit longer in order to see how it behaves with card!',
+      'example 2 is ex 2 ',
+      "example 3 we'll see, need to use the ... operator"
+    ],
+    contribution: {
+      name: 'Shpati',
+      username: 'spahiush',
+      timeOf: '15.04.1999'
+    }
   }
-  
+
   return (
     <div className='home'>
       <WordCard
-        title={title}
-        explanation={arrOfExplenations}
-        example={example}
-        contributor={mockPerson}
+        title={card?.title}
+        examples={card?.arrOfExamples}
+        explanation={card?.explanation}
+        contribution={card?.contribution}
+      />
+      <WordCard
+        title={card?.title}
+        examples={card?.arrOfExamples}
+        explanation={card?.explanation}
+        contribution={card?.contribution}
+      />
+      <WordCard
+        title={card?.title}
+        examples={card?.arrOfExamples}
+        explanation={card?.explanation}
+        contribution={card?.contribution}
       />
     </div>
   )
