eda319d16367cda3a544afbe88d1daec0ec35115
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-23 14:29:18 +0100

    Create dropdown menu

--- src/components/atoms/Navmenu/index.jsx
+++ src/components/atoms/Navmenu/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navmenu.scss'
 
 const Navmenu = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
     <div className='navmenu'>
-      <div className='menu'>
+      <div onClick={handleMenuClick} className='menu'>
         <svg
           width='32'
           height='32'
@@ -21,6 +27,12 @@ const Navmenu = () => {
           <path d='M4 18l16 0' />
         </svg>
       </div>
+
+      {showMenu && (
+        <div className='option-container'>
+          <div className='option'>Home</div>
+        </div>
+      )}
     </div>
   )
 }
--- src/components/pages/LayoutClient/index.jsx
+++ src/components/pages/LayoutClient/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../../molecules/Navbar'
 import { Outlet } from 'react-router-dom'
+import './LayoutClient.scss'
 
 const LayoutClient = () => {
   return (
-    <div>
+    <div className='layoutClient'>
       <Navbar />
       <Outlet />
     </div>
