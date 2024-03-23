c0deac2b897f24736f0d775dff3353f1dfa44d72
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-23 17:08:10 +0100

    Add navmenu icons

--- src/components/atoms/Navmenu/index.jsx
+++ src/components/atoms/Navmenu/index.jsx
@@ -32,13 +32,68 @@ const Navmenu = () => {
       {showMenu && (
         <div className='option-container'>
           <Link to='/'>
-            <div className='option'>Te shpija!</div>
+            <div className='option'>
+              <svg
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M5 12l-2 0l9 -9l9 9l-2 0' />
+                <path d='M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7' />
+                <path d='M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6' />
+              </svg>
+              Te shpija!
+            </div>
           </Link>
           <Link to='/about'>
-            <div className='option'>Cka o ky sen?</div>
+            <div className='option'>
+              <svg
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z' />
+                <path d='M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z' />
+                <path d='M5 8h4' />
+                <path d='M9 16h4' />
+                <path d='M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041z' />
+                <path d='M14 9l4 -1' />
+                <path d='M16 16l3.923 -.98' />
+              </svg>
+              Cka o ky sen?
+            </div>
           </Link>
           <Link to='/new-word'>
-            <div className='option'>Shto definicion...</div>
+            <div className='option'>
+              <svg
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z' />
+                <path d='M15 12h-6' />
+                <path d='M12 9v6' />
+              </svg>
+              Shto definicion...
+            </div>
           </Link>
         </div>
       )}
