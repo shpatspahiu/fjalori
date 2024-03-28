ffcdd4d9c64ff9c937588ce3ea1c7c7c8f876c78
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-29 00:15:08 +0100

    Create and style register page

--- src/components/atoms/Navmenu/index.jsx
+++ src/components/atoms/Navmenu/index.jsx
@@ -123,8 +123,25 @@ const Navmenu = () => {
               Shto definicion...
             </div>
           </Link>
-          <Link to='login'>
-            <div className='option'>Login</div>
+          <Link to='login' onClick={toggleMenu}>
+            <div className='option'>
+              <svg
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M15 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2' />
+                <path d='M21 12h-13l3 -3' />
+                <path d='M11 15l-3 -3' />
+              </svg>
+              Kyçu
+            </div>
           </Link>
         </div>
       )}
