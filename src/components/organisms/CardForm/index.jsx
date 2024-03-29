f3534a152859ea34ab28274b99ef31e23627ae37
Shpat Spahiu <shpat.spahiu@proton.me>
2024-03-29 01:00:08 +0100

    Create New CardForm and style it

--- src/components/organisms/CardForm/index.jsx
+++ src/components/organisms/CardForm/index.jsx
@@ -1,22 +1,90 @@
+import React, { useState } from 'react'
 import './CardForm.scss'
 
-import React from 'react'
-
 // eslint-disable-next-line
-const result = {
-  title: 'More pshtjellan',
-  explanation:
-    'Njeri qe genjen, tu perzi muhabete tulifare, pshtjelle pshtjelle, ta hup realitetin e gjanave si profi!',
-  examples: [
-    'Nejta ni dit me to, ama shum pshtjellan ke, krejt tjeter i kallxojke gjanat',
-    'Uuu pshtjellani nihere, aj e ka kallxu veq ni pjese trealitetit!',
-    ''
-  ],
-  contribution: 'Object id from mongodb of the user doing the post'
-}
+// const result = {
+//   title: 'More pshtjellan',
+//   explanation:
+//     'Njeri qe genjen, tu perzi muhabete tulifare, pshtjelle pshtjelle, ta hup realitetin e gjanave si profi!',
+//   examples: [
+//     'Nejta ni dit me to, ama shum pshtjellan ke, krejt tjeter i kallxojke gjanat',
+//     'Uuu pshtjellani nihere, aj e ka kallxu veq ni pjese trealitetit!',
+//     ''
+//   ],
+//   contribution: 'Object id from mongodb of the user doing the post'
+// }
 
 const CardForm = () => {
-  return <div className='cardForm'>CardForm</div>
+  const [card, setCard] = useState({
+    title: '',
+    explanation: '',
+    examples: '',
+    contributor: 'userId'
+  })
+
+  const changeHandler = e => {
+    setCard({
+      ...card,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  return (
+    <div className='cardForm'>
+      <div className='form'>
+        <input
+          type='text'
+          name='title'
+          id='title'
+          placeholder='Fjala e re'
+          onChange={changeHandler}
+          value={card?.title}
+        />
+
+        <textarea
+          placeholder='Shpjegimi i fjalës...'
+          id='explanation'
+          name='explanation'
+          rows='4'
+          cols='50'
+          onChange={changeHandler}
+        />
+
+        <textarea
+          placeholder="Shembull i përdorimit t'fjalës..."
+          id='examples'
+          name='examples'
+          rows='4'
+          cols='50'
+          onChange={changeHandler}
+        />
+
+        <div
+          onClick={() => {
+            console.log(card)
+          }}
+          className='submitBtn'
+        >
+          Dërgo për shqyrtim{' '}
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+            <path d='M5 12l14 0' />
+            <path d='M13 18l6 -6' />
+            <path d='M13 6l6 6' />
+          </svg>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CardForm
